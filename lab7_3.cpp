54365be9a0d6dc26a71f2d0b1710edaace639a40
Dexshine <b_o_y_s@hotmail.com>
2020-01-21 15:18:58 +0700

    ver1

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,31 @@
-//status = "ONE-NIGHT-STAND";
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age,height,money;
+    string status;
+    cout << "Input age: ";
+    cin >> age;
+    if(age<20){
+        cout << "Input height: ";
+        cin >> height;
+        if(height<160){ status = "UNFRIEND" ; }
+        else if(height<175){ status = "FRIEND"; }
+        else{
+            cout << "Input money: ";
+            cin >> money;
+            if(money>69000000){ status = "MARRIED"; }
+            else{ status = "ONE-NIGHT-STAND"; }
+
+        }
+
+    }else if(age<30){
+        cout << "Input money: ";
+        cin >> money;
+        if(money>10000000){ status = "BEST FRIEND"; }
+        else{ status = "UNFRIEND" ; }
+
+    }else{ status = "UNFRIEND"; }
+    cout << status;
+    return 0;
+}
